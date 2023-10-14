965a79a9a23741e80cd9855a2552711d94490d43
Ramsi9 <145783588+Ramsi9@users.noreply.github.com>
2023-10-14 20:26:09 +0300

    dz 9.2

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+        print();
+
     }
+    private static void print () {
+        System.out.println("Введите число:");
+        int input = new Scanner(System.in).nextInt();
+        if (input % 2 == 0) ;
+        {
+            System.out.println(input % 2 == 0);
+        }
+    }
+
 }
+
